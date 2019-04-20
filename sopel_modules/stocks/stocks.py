216918627da70ba2488ec3c6a3dac8235c9ece9d
Rusty Bower <rusty@rustybower.com>
2019-04-19 22:15:30 -0500

    Fixing bug with new stocks

--- sopel_modules/stocks/stocks.py
+++ sopel_modules/stocks/stocks.py
@@ -62,19 +62,25 @@ def stock(bot, trigger):
 
         days = sorted(data['Time Series (Daily)'].keys(), reverse=True)
 
-        # Get today's entry
-        today = days[0]
-        prevdate = days[1]
+        # Only 1 day of history, likely this is a new stock on the market
+        if len(days) == 1:
+            today = days[0]
+            close = data['Time Series (Daily)'][today]['4. close']
+            prevclose = data['Time Series (Daily)'][today]['1. open']
+        else:
+            # Get today's entry
+            today = days[0]
+            prevdate = days[1]
 
-        # dict_keys(['1. open', '2. high', '3. low', '4. close', '5. volume'])
-        # open = data['Time Series (Daily)'][today]['1. open']
-        # high = data['Time Series (Daily)'][today]['2. high']
-        # low = data['Time Series (Daily)'][today]['3. low']
-        close = data['Time Series (Daily)'][today]['4. close']
-        # volume = data['Time Series (Daily)'][today]['5. volume']
+            # dict_keys(['1. open', '2. high', '3. low', '4. close', '5. volume'])
+            # open = data['Time Series (Daily)'][today]['1. open']
+            # high = data['Time Series (Daily)'][today]['2. high']
+            # low = data['Time Series (Daily)'][today]['3. low']
+            close = data['Time Series (Daily)'][today]['4. close']
+            # volume = data['Time Series (Daily)'][today]['5. volume']
 
-        # Get yesterday's close
-        prevclose = data['Time Series (Daily)'][prevdate]['4. close']
+            # Get yesterday's close
+            prevclose = data['Time Series (Daily)'][prevdate]['4. close']
 
         # Calculate change
         change = float(close) - float(prevclose)
