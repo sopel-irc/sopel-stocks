3382a0de2b97fd6cac52ff4fd8b16456a40e135c
Rusty Bower <rusty@rustybower.com>
2022-11-12 12:23:48 -0600

    bumping to v1.2.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('tests/requirements.txt') as dev_requirements_file:
 
 setup(
     name='sopel_modules.stocks',
-    version='1.2.0',
+    version='1.2.1',
     description='Sopel Stocks Plugin',
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/markdown',  # This is important!
